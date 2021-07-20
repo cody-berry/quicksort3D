55a9adad2463b1d2df097c4d28a35bc592d164f2
Cody <cody.tian@icloud.com>
2021-07-20 14:13:30 -0700

    Clean up spaghetti code, add comments, add constants. Several bugs

--- Element.py
+++ Element.py
@@ -2,11 +2,14 @@
 
 class Element:
     def __init__(self, value):
-        self.c = color(13, 2, 99)
+        self.c = color(13, 2, 99, 40)
         self.value = value
+        # the done variable represents if it is sorted
+        # we assume that it is false during the beginning of the sort                      
+        self.done = False
         
     def reset_color(self):
-        self.c = color(13, 2, 99)
+        self.c = color(13, 2, 99, 40)
         
     def __lt__(self, other):
         return self.value < other.value
